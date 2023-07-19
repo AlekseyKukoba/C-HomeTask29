638273ccd414242902433cd28a094bb170e13022
Aleksey <kukoba.alexey@gmail.com>
2023-07-19 12:05:04 +0300

    final

--- Program.cs
+++ Program.cs
@@ -8,3 +8,21 @@ int Prompt(string message)
     int result = int.Parse(Console.ReadLine()!);
     return result;
 }
+int[] FillArray(int[] a) //Заполняем массив
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        a[i] = new Random().Next(0, 8);
+    }
+    return a;
+}
+void PrintArray(int[] a) //Печатаем массив
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.Write($"{a[i]} ");
+    }
+}
+int N = Prompt("Введите длиину массива: ");
+int[] a = new int[N];
+PrintArray(FillArray(a)); //Вызываем функцию заполнения массива внутри функции печати
